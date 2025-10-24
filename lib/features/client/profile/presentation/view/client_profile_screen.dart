22ef4545b521bf587217c952523c0203ec47eabb
esdawood <esoodawood186@gmail.com>
2025-10-24 23:50:21 +0300

    refactor: Add pull-to-refresh to client profile screen
    - Wrapped the `CustomScrollView` in a `RefreshIndicator` to allow users to refresh their profile information.
    - The `onRefresh` action triggers the `getProfile` method in `ProfileCubit` to refetch user data.
    - Configured the `RefreshIndicator` with a `darkBlue` color for a consistent UI.

--- lib/features/client/profile/presentation/view/client_profile_screen.dart
+++ lib/features/client/profile/presentation/view/client_profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freelancer/features/client/profile/presentation/manager/profile_cubit.dart';
 import '../../../../../core/di/injection_container.dart';
+import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/widgets/custom_header.dart';
 import '../../../../../generated/locale_keys.g.dart';
 import 'widgets/profile_content.dart';
@@ -14,21 +15,37 @@ class ClientProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => sl<ProfileCubit>(),
-      child: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomHeader(
-                  title: LocaleKeys.bottomNavigationBar_profile.tr(),
-                  isBack: false,
+      child: Builder(
+        builder: (context) {
+          return RefreshIndicator(
+            edgeOffset: 5,
+            onRefresh: () async {
+              await Future.delayed(const Duration(seconds: 2));
+              if (context.mounted) {
+                context.read<ProfileCubit>().getProfile();
+              }
+            },
+            backgroundColor: Colors.white,
+            color: AppColors.darkBlue,
+            child: CustomScrollView(
+              physics: const AlwaysScrollableScrollPhysics(),
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomHeader(
+                        title: LocaleKeys.bottomNavigationBar_profile.tr(),
+                        isBack: false,
+                      ),
+                      ProfileContent(),
+                    ],
+                  ),
                 ),
-                ProfileContent(),
               ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
